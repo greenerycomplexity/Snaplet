acee87a209955a8e337f27d9af9bf738963d3615
Mason <caohaison123@gmail.com>
2023-07-11 11:01:49 +1000

    Initial Commit

---
+++ Luminar/LuminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminarApp.swift
+//  Luminar
+//
+//  Created by Mason Cao on 11/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct LuminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
