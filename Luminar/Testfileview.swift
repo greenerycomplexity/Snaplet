7fa831986daf064077ec3efb2049ffc27826e478
Lewis Watson <lwpsare123@gmail.com>
2023-07-12 12:19:24 +1000

    new test view

---
+++ Luminar/Testfileview.swift
@@ -0,0 +1,20 @@
+//
+//  Testfileview.swift
+//  Luminar
+//
+//  Created by Lewis Watson  on 12/7/2023.
+//
+
+import SwiftUI
+
+struct Testfileview: View {
+    var body: some View {
+        Text("TEst2")
+    }
+}
+
+struct Testfileview_Previews: PreviewProvider {
+    static var previews: some View {
+        Testfileview()
+    }
+}
