1c6892e6fe772dfa43bb3d81ad48fe45945b7871
Mason <caohaison123@gmail.com>
2023-07-13 12:15:42 +1000

    Static home view (Animation pending)

--- Luminar/HomeView.swift
+++ Luminar/HomeView.swift
@@ -9,36 +9,70 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var isPopoverShown:Bool = true
+    @State private var isPopoverShown:Bool = false
     
     var body: some View {
-        
-        VStack{
-            
-            
-            NavigationStack{
+        NavigationStack{
+            ZStack{
+                Color("creamyCream").ignoresSafeArea()
                 
-                Text("Prompt of the week")
-                    .font(.title2)
-
-                NavigationLink{
-                    CameraView(onCaptureComplete: {}, dismissAutomaticallyOnCapture: true)
-                        .navigationBarBackButtonHidden(true)
-                } label: {
-                    Image(systemName: "camera.circle")
-                        .foregroundColor(.red)
-                        .font(.system(size: 40))
+                VStack{
+                    
+                    Image("starbucks_example")
+                        .resizable()
+                        .cornerRadius(15.0)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 250)
+                        .offset(x: 50, y:15)
+                    
+                    
+                    Spacer()
+                    
+                    Text("Prompt of the week")
+                        .font(.title2)
+                        .bold()
+                    
+                    Image("breakfast_example")
+                        .resizable()
+                        .cornerRadius(15.0)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 250)
+                        .offset(x: -50, y:15)
+                    
+    
+                    
+                    
+                    
+                    Spacer()
+                    
+                    NavigationLink ("Start"){
+                        CameraView(onCaptureComplete: {}, dismissAutomaticallyOnCapture: true)
+                            .navigationBarBackButtonHidden(true)
+                    } .buttonStyle(AccentButton())
+                    
+                    
                 }
                 
-                
             }
-
-        }
-        .popover(isPresented: $isPopoverShown) {
+            .popover(isPresented: $isPopoverShown) {
+                
+                WelcomeFirst(isPopoverShown: $isPopoverShown)
+            }
             
-            WelcomeFirst(isPopoverShown: $isPopoverShown)
         }
         
+    }
+    
+}
+
+
+struct AccentButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("ponyPink"))
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
     }
 }
 
